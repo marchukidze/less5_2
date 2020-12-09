01598d0c8cad8329b0ab8cb2e7b179c0f4575b4b
marchukidze <64844669+marchukidze@users.noreply.github.com>
2020-12-09 14:20:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,37 +3,66 @@ var str2 = '!!_gla_5u7g29_/!g!';
 var str3 = 'g!t78**#g#/gg!ee!cd!_k_?';
 var restrictedList = ['!', 'g', '_', '/'];
 
-var str1 = str1.split('');
-var str2 = str2.split('');
-var str3 = str3.split('');
-
-for (var i = 0; i < str1.length; i++) {
-    for (var j = 0; j < restrictedList.length; j++) {
-        if (str1[i] == restrictedList[j]) {
-            str1.splice(i, 1);
-            i--;
+function restr(str) {
+    var str = str.split('')
+    for (var i = 0; i < str.length; i++) {
+        for (var j = 0; j < restrictedList.length; j++) {
+            if (str[i] == restrictedList[j]) {
+                str.splice(i, 1);
+                i--;
+            }
         }
     }
+    return str.join('');
 }
 
-for (var i = 0; i < str2.length; i++) {
-    for (var j = 0; j < restrictedList.length; j++) {
-        if (str2[i] == restrictedList[j]) {
-            str2.splice(i, 1);
-            i--;
-        }
-    }
-}
+str1 = restr(str1);
+str2 = restr(str2);
+str3 = restr(str3);
 
-for (var i = 0; i < str3.length; i++) {
-    for (var j = 0; j < restrictedList.length; j++) {
-        if (str3[i] == restrictedList[j]) {
-            str3.splice(i, 1);
-            i--;
-        }
-    }
-}
+console.log(str1);
+console.log(str2);
+console.log(str3);
 
-console.log(str1.join(''));
-console.log(str2.join(''));
-console.log(str3.join(''));
+
+//////////////
+
+// var str1 = '/76gk_g6747ggf/!!';
+// var str2 = '!!_gla_5u7g29_/!g!';
+// var str3 = 'g!t78**#g#/gg!ee!cd!_k_?';
+// var restrictedList = ['!', 'g', '_', '/'];
+
+// var str1 = str1.split('');
+// var str2 = str2.split('');
+// var str3 = str3.split('');
+
+// for (var i = 0; i < str1.length; i++) {
+//     for (var j = 0; j < restrictedList.length; j++) {
+//         if (str1[i] == restrictedList[j]) {
+//             str1.splice(i, 1);
+//             i--;
+//         }
+//     }
+// }
+
+// for (var i = 0; i < str2.length; i++) {
+//     for (var j = 0; j < restrictedList.length; j++) {
+//         if (str2[i] == restrictedList[j]) {
+//             str2.splice(i, 1);
+//             i--;
+//         }
+//     }
+// }
+
+// for (var i = 0; i < str3.length; i++) {
+//     for (var j = 0; j < restrictedList.length; j++) {
+//         if (str3[i] == restrictedList[j]) {
+//             str3.splice(i, 1);
+//             i--;
+//         }
+//     }
+// }
+
+// console.log(str1.join(''));
+// console.log(str2.join(''));
+// console.log(str3.join(''));
